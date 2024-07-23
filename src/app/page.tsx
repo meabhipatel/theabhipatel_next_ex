04ffb1e948796d32de4f890a14e2d5f811b37b9e
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-07-23 18:33:41 +0530

    testing prettiere eslint configuration 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,36 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home() {
 
-const variable =''
+
+
+
+
+
+
+
+
+  
+  const variable = "";
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex    ">
-
-
-
-
+    <main className="           flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex     ">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:dark:bg-zinc-800/30">
           A Full Stack Developer
-          <code className="ml-2 font-mono font-bold">Portfolio.tsx</code>
+          <code className="                                                                                                ml-2 font-mono font-bold">Portfolio.tsx</code>
         </p>
+
+
 
 
 
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:size-auto lg:bg-none dark:from-black dark:via-black">
           <Link
+
+
+
+
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://theabhipatel.com"
             target="_blank"
@@ -34,6 +45,23 @@ const variable =''
         <h1 className="text-5xl font-semibold text-blue-100">TheAbhiPatel</h1>
       </div>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <Link
           href="/about"
@@ -46,7 +74,7 @@ const variable =''
               -&gt;
             </span>
           </h2>
-          <p className="         m-0 max-w-[30ch] text-sm opacity-50">
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Find in-depth information about Next.js features and API.
           </p>
         </Link>
@@ -56,6 +84,24 @@ const variable =''
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
           <h2 className="mb-3 text-2xl font-semibold">
             Products
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -81,6 +127,38 @@ const variable =''
           <p className="m-0 max-w-[30ch] text-sm opacity-50">Explore starter templates for Next.js.</p>
         </Link>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         <Link
           href={"contact"}
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -92,32 +170,6 @@ const variable =''
               -&gt;
             </span>
           </h2>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
             Instantly deploy your Next.js site to a shareable URL with Vercel.
