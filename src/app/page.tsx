49c79ef9140a217a32037481bb9406d856e9aa1b
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-07-24 10:36:49 +0530

    testing eslint and prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,18 @@
 import Link from "next/link";
 
 export default function Home() {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-  
   const variable = "";
 
-  
-
   return (
-    <main className="           flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex     ">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:dark:bg-zinc-800/30">
           A Full Stack Developer
-          <code className="                                                                                                ml-2 font-mono font-bold">Portfolio.tsx</code>
+          <code className="ml-2 font-mono font-bold">Portfolio.tsx</code>
         </p>
-
-
-
-
 
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:size-auto lg:bg-none dark:from-black dark:via-black">
           <Link
-
-
-
-
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://theabhipatel.com"
             target="_blank"
@@ -65,23 +26,6 @@ export default function Home() {
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-[#2f2ce9] after:dark:via-[#ff0141] after:dark:opacity-90">
         <h1 className="text-5xl font-semibold text-blue-100">TheAbhiPatel</h1>
       </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <Link
@@ -105,24 +49,6 @@ export default function Home() {
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
           <h2 className="mb-3 text-2xl font-semibold">
             Products
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -147,38 +73,6 @@ export default function Home() {
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">Explore starter templates for Next.js.</p>
         </Link>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         <Link
           href={"contact"}
