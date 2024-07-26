6f9a8c34fac9d15273bf84b9d967b6d316a95872
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-07-26 17:55:54 +0530

    chore commented navbar import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import { ReactNode } from "react";
+// import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,10 +14,13 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className="dark"
+    >
       <body className={inter.className}>{children}</body>
     </html>
   );
