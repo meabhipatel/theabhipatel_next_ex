041eaee0feb691facb2d62dc9441396bfe7233fb
Abhishek Patel <162666031+meabhipatel@users.noreply.github.com>
2024-08-06 14:20:56 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       </div>
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-[#2f2ce9] after:dark:via-[#ff0141] after:dark:opacity-90">
-        <h1 className="text-5xl font-semibold bg-gradient-to-b from-blue-300 to-blue-50 bg-clip-text text-transparent ">TheAbhiPatel</h1>
+        <h1 className="text-5xl font-semibold bg-gradient-to-t from-blue-300 via-blue-100 to-blue-50 bg-clip-text text-transparent ">TheAbhiPatel</h1>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
